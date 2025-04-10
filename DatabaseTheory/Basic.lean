53ca9311e1966103a6cae70fff07e65e881a3048
monsterkrampe <monsterkrampe@users.noreply.github.com>
2025-04-10 13:59:07 +0200

    Add Lean Definitions for Relational Algbra Ops

--- DatabaseTheory/Basic.lean
+++ DatabaseTheory/Basic.lean
@@ -1,1 +1,201 @@
-def hello := "world"
+-- Many basic definitions are taken from (or at least inspired by) https://lean-lang.org/functional_programming_in_lean/dependent-types/typed-queries.html
+
+inductive DBType where
+| int | string | bool
+deriving DecidableEq
+
+abbrev DBType.asType : DBType → Type
+| .int => Int
+| .string => String
+| .bool => Bool
+
+def DBType.beq (t : DBType) (x y : t.asType) : Bool :=
+  match t with
+  | .int => x == y
+  | .string => x == y
+  | .bool => x == y
+
+instance {t : DBType} : BEq t.asType where
+  beq := t.beq
+
+structure SchemaCol where
+  name : String
+  dbType : DBType
+deriving DecidableEq
+
+abbrev TableSchema := List SchemaCol
+
+abbrev Row : TableSchema -> Type
+| [] => Unit
+| [col] => col.dbType.asType
+| col1 :: col2 :: cols => col1.dbType.asType × Row (col2::cols)
+
+def Row.bEq (r1 r2 : Row s) : Bool :=
+  match s with
+  | [] => true
+  | [_] => r1 == r2
+  | _::_::_ =>
+    match r1, r2 with
+    | (v1, r1'), (v2, r2') =>
+      v1 == v2 && bEq r1' r2'
+
+instance : BEq (Row s) where
+  beq := Row.bEq
+
+abbrev Table (s : TableSchema) := List (Row s)
+
+def Table.distinct (t : Table s) : Table s := t.eraseDups
+
+inductive HasCol : TableSchema → String → DBType → Type where
+| here : HasCol (⟨name, t⟩ :: _) name t
+| there : HasCol s name t → HasCol (_ :: s) name t
+
+def TableSchema.has_col (s : TableSchema) (col : SchemaCol) : Option (HasCol s col.name col.dbType) :=
+  match s with
+  | [] => none
+  | c :: s =>
+    if h : c = col
+    then .some (cast (by rw [h]) (@HasCol.here s c.name c.dbType))
+    else
+      let res := TableSchema.has_col s col
+      res.map (fun hasCol => .there hasCol)
+
+theorem TableSchema.has_col_isSome_of_mem (s : TableSchema) (col : SchemaCol) : col ∈ s -> (s.has_col col).isSome := by
+  induction s with
+  | nil => simp
+  | cons hd tl ih =>
+    rw [List.mem_cons]
+    intro h
+    simp only [has_col]
+    cases Decidable.em (hd = col) with
+    | inl eq => simp [eq]
+    | inr neq =>
+      simp only [neq, Bool.false_eq_true, ↓reduceDIte, Option.isSome_map']
+      apply ih
+      cases h with
+      | inl h => rw [h] at neq; simp at neq
+      | inr h => exact h
+
+def Row.get (row : Row s) (col : HasCol s n t) : t.asType :=
+  match s, col, row with
+  | [_], .here, v => v
+  | _::_::_, .here, (v, _) => v
+  | _::_::_, .there next, (_, r) => get r next
+
+inductive Subschema : TableSchema → TableSchema → Type where
+  | nil : Subschema [] bigger
+  | cons :
+      HasCol bigger n t →
+      Subschema smaller bigger →
+      Subschema (⟨n, t⟩ :: smaller) bigger
+
+def Subschema.addColumn (sub : Subschema smaller bigger) : Subschema smaller (c :: bigger) :=
+  match sub with
+  | .nil  => .nil
+  | .cons col sub' => .cons (.there col) sub'.addColumn
+
+def Subschema.reflexive : (s : TableSchema) → Subschema s s
+  | [] => .nil
+  | _ :: cs => .cons .here (reflexive cs).addColumn
+
+def Row.project (row : Row s) : (s' : TableSchema) → Subschema s' s → Row s'
+  | [], .nil => ()
+  | [_], .cons c .nil => row.get c
+  | _::_::_, .cons c cs => (row.get c, row.project _ cs)
+
+def Table.project (table : Table s) (s' : TableSchema) (sub : Subschema s' s) : Table s' := table.map (fun row => row.project s' sub)
+
+inductive SelectionArgument (left : HasCol s n t) where
+| const : t.asType -> SelectionArgument left
+| col : HasCol s n2 t -> SelectionArgument left
+
+def Row.select (row : Row s) (left : HasCol s n t) (arg : SelectionArgument left) : Bool :=
+  match arg with
+  | .const c => row.get left == c
+  | .col right => row.get left == row.get right
+
+def Table.select (table : Table s) (left : HasCol s n t) (arg : SelectionArgument left) : Table s := table.filter (fun row => row.select left arg)
+
+def TableSchema.renameColumn : (s : TableSchema) → HasCol s n t → String → TableSchema
+| c :: cs, .here, n' => {c with name := n'} :: cs
+| c :: cs, .there next, n' => c :: renameColumn cs next n'
+
+def Row.addVal (v : c.dbType.asType) (row : Row s) : Row (c :: s) :=
+  match s, row with
+  | [], () => v
+  | _ :: _, v' => (v, v')
+
+def Row.rename (row : Row s) (col : HasCol s n t) (newName : String) : Row (s.renameColumn col newName) :=
+  match s, row, col with
+  | [_], v, .here => v
+  | _::_::_, (v, r), .here => addVal v r
+  | _::_::_, (v, r), .there next => addVal v (r.rename next newName)
+
+def Table.rename (t : Table s) (col : HasCol s n ty) (newName : String) : Table (s.renameColumn col newName) :=
+  t.map (fun r => r.rename col newName)
+
+def Table.diff (t1 t2 : Table s) : Table s := t1.filter (fun r => !t2.contains r)
+
+def Table.union (t1 t2 : Table s) : Table s := t1 ++ t2
+
+def Table.intersection (t1 t2 : Table s) : Table s := t1.filter (fun r => t2.contains r)
+
+
+-- Natural Join starts here - couple of ingredients needed
+
+def TableSchema.dropColumns (s : TableSchema) (cols : TableSchema) : TableSchema := s.filter (fun c => ¬ c ∈ cols)
+
+def Subschema.dropColumns (s: TableSchema) (cols : TableSchema) : Subschema (s.dropColumns cols) s :=
+  match s with
+  | [] => .nil
+  | hd::tl =>
+    let sub : Subschema (TableSchema.dropColumns tl cols) (hd::tl) := Subschema.addColumn (Subschema.dropColumns tl cols)
+    if h : hd ∈ cols
+    then
+      cast (by
+        unfold TableSchema.dropColumns
+        rw [List.filter_cons]
+        simp [h]
+      ) sub
+    else
+      cast (by
+        unfold TableSchema.dropColumns
+        rw [List.filter_cons]
+        simp [h]
+      ) (Subschema.cons HasCol.here sub)
+
+def Row.dropColumns (row : Row s) (cols : TableSchema) : Row (s.dropColumns cols) :=
+  row.project _ (Subschema.dropColumns s cols)
+
+def Row.concat (r1 : Row s1) (r2 : Row s2) : Row (s1 ++ s2) :=
+  match s1, r1 with
+  | [], () => r2
+  | [_], v => addVal v r2
+  | _::_::_, (v, r1) => addVal v (concat r1 r2)
+
+def Row.colsMatch (r1 : Row s1) (r2 : Row s2) (left : HasCol s1 n t) (right : HasCol s2 n t) : Bool :=
+  r1.get left == r2.get right
+
+def Row.matchesOnCol (r1 : Row s1) (r2 : Row s2) (col : SchemaCol) : Bool :=
+  match s1.has_col col with
+  | .none => false
+  | .some left => match s2.has_col col with
+    | .none => false
+    | .some right => colsMatch r1 r2 left right
+
+def Row.joinOn (r1 : Row s1) (r2 : Row s2) (joinCols : TableSchema) : Option (Row (s1 ++ s2.dropColumns joinCols)) :=
+  if joinCols.all (fun col => Row.matchesOnCol r1 r2 col)
+  then .some (r1.concat (r2.dropColumns joinCols))
+  else .none
+
+def Row.naturalJoin (r1 : Row s1) (r2 : Row s2) : Option (Row (s1 ++ s2.dropColumns (s1.filter (fun c => c ∈ s2)))) :=
+  let joinCols := s1.filter (fun c => c ∈ s2)
+  Row.joinOn r1 r2 joinCols
+
+def Table.naturalJoin (t1 : Table s1) (t2 : Table s2) : Table (s1 ++ s2.dropColumns (s1.filter (fun c => c ∈ s2))) :=
+  t1.flatMap (fun r1 =>
+    t2.filterMap (fun r2 =>
+      r1.naturalJoin r2
+    )
+  )
+
---
+++ DatabaseTheory/Exercise01.lean
@@ -0,0 +1,109 @@
+import DatabaseTheory.Basic
+
+def S_Films : TableSchema := [
+  { name := "Title", dbType := DBType.string },
+  { name := "Director", dbType := DBType.string },
+  { name := "Actor", dbType := DBType.string },
+]
+
+def S_Venues : TableSchema := [
+  { name := "Cinema", dbType := DBType.string },
+  { name := "Address", dbType := DBType.string },
+  { name := "Phone", dbType := DBType.string },
+]
+
+def S_Program : TableSchema := [
+  { name := "Cinema", dbType := DBType.string },
+  { name := "Title", dbType := DBType.string },
+  { name := "Time", dbType := DBType.string },
+]
+
+def T_Films : Table S_Films := [
+  ("The Imitation Game", "Tyldum", "Cumberbatch"),
+  ("The Imitation Game", "Tyldum", "Knightley"),
+  ("The Internet's Own Boy", "Knappenberger", "Swartz"),
+  ("The Internet's Own Boy", "Knappenberger", "Lessig"),
+  ("The Internet's Own Boy", "Knappenberger", "Berners-Lee"),
+  ("Dogma", "Smith", "Damon"),
+  ("Dogma", "Smith", "Affleck"),
+  ("Dogma", "Smith", "Morissette"),
+  ("Dogma", "Smith", "Smith"),
+  ("Film by Smith I made up", "Smith", "Damon"),
+  ("Film by Smith I made up", "Smith", "Morissette"),
+  ("Film not by Smith I made up", "Not Smith", "Damon"),
+  ("Film by Knappenberger I made up", "Knappenberger", "Cumberbatch"),
+  ("Film by Knappenberger I made up", "Knappenberger", "Knightley"),
+  ("Film by Knappenberger I made up", "Knappenberger", "Damon"),
+  ("Film by Knappenberger I made up", "Knappenberger", "Affleck"),
+  ("Film by Knappenberger I made up", "Knappenberger", "Morissette"),
+  ("Film by Knappenberger I made up", "Knappenberger", "Smith"),
+]
+
+def T_Venues : Table S_Venues := [
+  ("UFA", "St. Petersburger Str. 24", "4825825"),
+  ("Schauburg", "St. Petersburger Str. 24", "8032185"),
+  ("CinemaxX", "Hüblerstr. 8", "3158910"),
+]
+
+def T_Program : Table S_Program := [
+  ("Schauburg", "The Imitation Game", "19:30"),
+  ("Schauburg", "Dogma", "20:45"),
+  ("Ufa", "The Imitation Game", "22:45"),
+  ("CinemaxX", "The Imitation Game", "19:30"),
+]
+
+-- Exercise 1.1
+
+-- 1. Director of Imitation Game
+#eval (T_Films.select .here (.const "The Imitation Game")).project _ (.cons (.there .here) .nil)
+
+-- 2. Cinemas with Imitation Game
+-- Note: instead of using here/there we obtain HasCol from the name of the column but this is also quite verbose
+#eval (T_Program.select ((S_Program.has_col { name := "Title", dbType := DBType.string }).get (by apply TableSchema.has_col_isSome_of_mem; decide)) (.const "The Imitation Game")).project _ (.cons .here .nil)
+
+-- 3. Address and Phone of Schauburg
+#eval (T_Venues.select .here (.const "Schauburg")).project _ (.cons (.there .here) (.cons (.there (.there .here)) .nil))
+
+-- 4. Film by Smith playing? (Empty List indicates false, List with any positive number of () indicates true)
+#eval ((T_Films.select (.there .here) (.const "Smith")).naturalJoin T_Program).project _ .nil
+
+-- 5. Director directed Actor and vice versa
+#eval ((((((T_Films.rename .here "T").rename (.there .here) "D").rename (.there (.there .here)) "A").naturalJoin T_Films).select (.there .here) (.col (.there (.there (.there (.there (.there .here))))))).select (.there (.there .here)) (.col (.there (.there (.there (.there .here)))))).project _ (.cons (.there .here) (.cons (.there (.there (.there (.there .here)))) .nil))
+
+-- 6. Directors acting in their film
+#eval (T_Films.select (.there .here) (.col (.there (.there .here)))).project _ (.cons (.there .here) .nil)
+
+-- 7. Fixed table (we allow constant tables with single columns here only)
+def T_Constant_Title : Table [{name := "Title", dbType := DBType.string}] := [("Apocalypse Now")]
+def T_Constant_Director : Table [{name := "Director", dbType := DBType.string}] := [("Coppola")]
+#eval T_Constant_Title.naturalJoin T_Constant_Director
+
+-- 8. Actors in Film by Smith
+#eval (T_Films.select (.there .here) (.const "Smith")).project _ (.cons (.there (.there .here)) .nil)
+
+-- 9. Actors in every film by smith
+-- aux: actors not in some film by smith
+def aux_table_for_9 := (((T_Films.project _ (.cons (.there (.there .here)) .nil)).naturalJoin ((T_Films.select (.there .here) (.const "Smith")).project _ (.cons .here .nil))).diff ((T_Films.select (.there .here) (.const "Smith")).project _ (.cons (.there (.there .here)) (.cons .here .nil)))).project _ (.cons .here .nil)
+#eval aux_table_for_9
+#eval (T_Films.project _ (.cons (.there (.there .here)) .nil)).diff aux_table_for_9
+
+-- 10. actors only in films by smith
+#eval (T_Films.project _ (.cons (.there (.there .here)) .nil)).diff ((T_Films.diff (T_Films.select (.there .here) (.const "Smith"))).project _ (.cons (.there (.there .here)) .nil))
+
+-- 11. actors acting together in at least one film
+#eval ((T_Films.rename (.there (.there .here)) "RA").naturalJoin T_Films).project _ (.cons (.there (.there (.there .here))) (.cons (.there (.there .here)) .nil))
+
+-- 12. actors cast in exactly the same films
+def aux_table_for_12_1_1 := ((T_Films.project _ (.cons (.there (.there .here)) .nil)).naturalJoin (T_Films.rename (.there (.there .here)) "RA")).project _ (.cons (.there .here) (.cons (.there (.there .here)) (.cons .here (.cons (.there (.there (.there .here))) .nil))))
+def aux_table_for_12_1_2 := T_Films.naturalJoin ((T_Films.project _ (.cons (.there (.there .here)) .nil)).rename .here "RA")
+def aux_table_for_12_1 := (aux_table_for_12_1_1.diff aux_table_for_12_1_2).project _ (.cons (.there (.there .here)) (.cons (.there (.there (.there .here))) .nil))
+/- #eval aux_table_for_12_1_1 -/
+/- #eval aux_table_for_12_1_2 -/
+#eval aux_table_for_12_1
+def aux_table_for_12_2 := ((T_Films.project _ (.cons (.there (.there .here)) .nil)).naturalJoin ((T_Films.project _ (.cons (.there (.there .here)) .nil)).rename .here "RA")).diff aux_table_for_12_1
+#eval aux_table_for_12_2
+#eval (aux_table_for_12_2.naturalJoin (((aux_table_for_12_2.rename .here "TMP").rename (.there .here) "Actor").rename .here "RA")).distinct
+
+-- 13. directors directing every actor
+#eval ((T_Films.project _ (.cons (.there .here) .nil)).diff ((((T_Films.project _ (.cons (.there .here) .nil)).naturalJoin (T_Films.project _ (.cons (.there (.there .here)) .nil))).diff (T_Films.project _ (.cons (.there .here) (.cons (.there (.there .here)) .nil)))).project _ (.cons .here .nil))).distinct
+
